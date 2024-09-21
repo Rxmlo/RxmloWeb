046512498b8c404c4bfe07df274d64a496c8f47e
Rxmlo <enssera47@gmail.com>
2024-09-21 14:56:09 +0300

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.com/oauth2/authorize?client_id=1284268171976773664">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/72826a4ed7d1a010df263623be5dd4ba.png" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1274439590832246804/1287016942686703648/image_-_2024-09-15T141929.083.png?ex=66f0036e&is=66eeb1ee&hm=5c754851b1d98e5bae6278eefa440ba46fc54655c1e8753a7b4b46c1f6cc32a5&=&format=webp&quality=lossless&width=423&height=423" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Mandsy</h1>        
                                 <div className="flex items-center mt-1">
@@ -33,14 +33,14 @@ export default function Partners() {
 
 
                             <div className="flex flex-col justify-center text-white rounded">
-            <a href="">
+            <a href="https://discord.com/oauth2/authorize?client_id=1284268171976773664">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1274439590832246804/1287016942686703648/image_-_2024-09-15T141929.083.png?ex=66f0036e&is=66eeb1ee&hm=5c754851b1d98e5bae6278eefa440ba46fc54655c1e8753a7b4b46c1f6cc32a5&=&format=webp&quality=lossless&width=423&height=423" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">YOK</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Mandsy</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                   sponsor bulunmamaktadır! olmak isteyen kişileri **destek sunucusu** na bekliyoruz!
+                                   Mandsy Herkese açık Discord botu!
                                     </p>
                                 </div>     
                             </div>
