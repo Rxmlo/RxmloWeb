92923a2acbdb5f895d6200c82a8b9d00191ce5f0
Rxmlo <enssera47@gmail.com>
2024-09-21 15:01:14 +0300

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,14 +16,14 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.com/oauth2/authorize?client_id=1284268171976773664">
+            <a href="https://discord.gg/vencode">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1274439590832246804/1287016942686703648/image_-_2024-09-15T141929.083.png?ex=66f0036e&is=66eeb1ee&hm=5c754851b1d98e5bae6278eefa440ba46fc54655c1e8753a7b4b46c1f6cc32a5&=&format=webp&quality=lossless&width=423&height=423" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1274439590832246804/1287020544197267529/Rxmlo-Logo.png?ex=66f006c9&is=66eeb549&hm=607a86eacfa5de5ad2cf96c75c80e0f90c3c34a17dd42fdbbbbda5d8150dc9fa&=&format=webp&quality=lossless" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Mandsy</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">YOK</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      Mandsy Herkese açık Discord botu!
+                                      Şu anlık yok!
                                     </p>
                                 </div>     
                             </div>
